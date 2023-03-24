1600086bb5565043c09263e58022aa594e500474
Pedro H <pedro_gonsalves@hotmail.com>
2023-03-24 14:55:44 -0300

    finish tecnologies table and operations

--- database/operations/tecnologies_operations.py
+++ database/operations/tecnologies_operations.py
@@ -1,13 +1,18 @@
-def insertTecnologiesDB():
+def insertTecnologiesDB() -> dict:
     from database.entities.tecnologies import Tecnologies
     from database.connection.connection import connection
     from sqlalchemy import insert
+    import requests
+    import io
     import pandas as pd
 
     engine, base, session = connection()
 
     dictFrame:dict = {}
-    dfTecnologies = pd.read_csv("../../datasets/tecnologies_jsa.csv",on_bad_lines='skip',sep=';')
+
+    res = requests.get("https://raw.githubusercontent.com/pedrohgoncalvess/jobanalytics/master/datasets/tecnologies_jsa.csv").content
+    dfTecnologies = pd.read_csv(io.StringIO(res.decode('utf-8')), on_bad_lines='skip',sep=';')
+
     for num,line in enumerate(dfTecnologies['Tecnology']):
         query = insert(Tecnologies).values(
             tecnologie = line.lower(),
@@ -21,3 +26,16 @@ def insertTecnologiesDB():
             session.close()
     return dictFrame
 
+def listTecnologies() -> dict:
+    from database.entities.tecnologies import Tecnologies
+    from database.connection.connection import connection
+
+    engine, base, session = connection()
+    query = session.query(Tecnologies).all()
+
+    dictTecnologies:dict = {}
+
+    for line in query:
+        dictTecnologies.update({line.tecnologie: line.type})
+
+    return dictTecnologies
